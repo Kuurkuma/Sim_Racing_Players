2c611a8ba5d73aae4ae9f1def58eb39f7247b2fa
Kuurkuma <t.ber@mailfence.com>
2024-04-29 18:04:54 +0200

    change data directory's path for the one in GitHub, also for the 2 others pages

--- scripts/pages/03_Twitch_viewers.py
+++ scripts/pages/03_Twitch_viewers.py
@@ -20,7 +20,7 @@ def load_data(data_path:str):
     data = pd.read_pickle(data_path)
     return data
 # Define dataframe
-df = load_data('/Users/macbook/Dropbox/Mac/Documents/Pro/Data_Analyst/simracing-players/data/interim/sim_racing_games-1.0.pkl')
+df = load_data('data/interim/sim_racing_games-1.0.pkl')
 
 #-----------------
 
